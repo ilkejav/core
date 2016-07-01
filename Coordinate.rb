3911e1a3582082b9f668e8a790d68b4228c2a35f
Devine Lu Linvega <aliceffekt@gmail.com>
2016-06-30 23:19:04 -0700

    Clean up

---
+++ Coordinate.rb
@@ -0,0 +1,45 @@
+class Coordinate
+  
+  def initialize lat, lon
+    @lon = lon
+    @lat = lat
+  end
+
+  def lat
+    return @lat
+  end
+
+  def lon
+    return @lon
+  end
+
+  def deg2rad
+    return PI / 180
+  end
+
+  def power(num, pow)
+    return num ** pow
+  end
+
+  def distanceFrom coord
+
+    lat1 = @lat * deg2rad
+    lon1 = @lon * deg2rad
+    lat2 = coord.lat * deg2rad
+    lon2 = coord.lon * deg2rad
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    # a = power(sin(dlat/2), 2) + cos(lat1) * cos(lat2) * power(sin(dlon/2), 2)
+
+    # a = ((sin(dlat/2))**2) + cos(lat1) * cos(lat2) * ((sin(dlon/2))**2)
+    # c = 6371 * 2 * atan2(sqrt(a), sqrt(1-a))
+
+    c = 2 * 6371 * asin(sqrt(sin((lat2-lat1)/2)**2 + cos(lat1) * cos(lat2) * sin((lon2 - lon1)/2)**2))
+
+    return c
+
+  end
+
+end
